03f7bbd924efd7a120953dda1959191ac2336ac4
Seyyed davood hosseini <hosseinydavid@gmail.com>
2021-02-11 14:54:43 +0330

    Refactor SmsListener

--- app/src/main/java/com/parsdroid/verifycodereader/SmsListener.kt
+++ app/src/main/java/com/parsdroid/verifycodereader/SmsListener.kt
@@ -1,6 +1,10 @@
 package com.parsdroid.verifycodereader
 
-import android.content.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION
 import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
@@ -10,26 +14,31 @@ class SmsListener : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
-        if (intent.action != "android.provider.Telephony.SMS_RECEIVED") {
+        if (intent.action != SMS_RECEIVED_ACTION) {
             return
         }
 
         intent.extras?.let { bundle ->
             try {
-                val pdus = bundle[PDUS] as Array<Any>?
-                var messageBody = ""
-                var msgFrom: String?
-                pdus?.forEach { pdu ->
-                    val message: SmsMessage? = SmsMessage.createFromPdu(pdu as ByteArray)
-                    msgFrom = message?.originatingAddress
-                    messageBody = message?.messageBody.orEmpty()
-                }
-                Log.d(TAG, messageBody)
+                @Suppress("UNCHECKED_CAST")
+                val pdus = bundle[PDU_TYPE] as? Array<ByteArray?> ?: return
+                val format = bundle.getString("format")
 
-                val verifyCode = getVerifyCodeFromSmsText(messageBody) ?: return
+                var tempVerifyCode: String? = null
+                val verifyCode = pdus.lastOrNull {
+                    val smsMessage = smsMessageFunction(it, format)
+                    val msgFrom = smsMessage?.originatingAddress
+                    val messageBody = smsMessage?.messageBody.orEmpty()
+                    Log.d(TAG, "$msgFrom: $messageBody")
+                    tempVerifyCode = getVerifyCodeFromSmsText(messageBody)
+                    tempVerifyCode != null
+                }?.let {
+                    tempVerifyCode
+                } ?: return
 
                 copyToClipboard(context, "VerifyCode", verifyCode)
-                Toast.makeText(context, "$verifyCode copied to clipboard", Toast.LENGTH_LONG)
+                Toast
+                    .makeText(context, "$verifyCode copied to clipboard", Toast.LENGTH_LONG)
                     .show()
             } catch (e: Exception) {
                 Log.e(TAG, e.message.toString())
@@ -37,17 +46,21 @@ class SmsListener : BroadcastReceiver() {
         }
     }
 
-    private fun copyToClipboard(context: Context, label: String, text: String) {
-        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText(label, text)
-        clipboard.setPrimaryClip(clip)
-    }
-
     companion object {
-        private const val PDUS = "pdus"
+
+        private const val PDU_TYPE = "pdus"
         private val KEYWORDS = arrayOf("رمز", "کد", "password", "code")
         private val TAG = SmsListener::class.java.simpleName
 
+        private val smsMessageFunction: (ByteArray?, String?) -> SmsMessage? =
+            if (isApiLevelAndUp(Build.VERSION_CODES.M)) {
+                SmsMessage::createFromPdu
+            } else {
+                { byteArray, _ ->
+                    @Suppress("DEPRECATION")
+                    SmsMessage.createFromPdu(byteArray)
+                }
+            }
 
         fun getVerifyCodeFromSmsText(smsText: String): String? {
             val words = smsText.trim().split("\\s+".toRegex())
